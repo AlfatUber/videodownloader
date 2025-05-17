9b1167c0cd87d58f056659e17da0c482335ed58c
AlfatUber <167688170+AlfatUber@users.noreply.github.com>
2025-05-17 08:56:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -78,6 +78,9 @@ async def download(
         "outtmpl": filename,
         "progress_hooks": [progress_hook],
         "quiet": True,
+         "http_headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+        }
     }
 
     if cookie_path:
