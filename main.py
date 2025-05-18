23478d1e39c1bdf64d8845fd88ac85da23bc1dc4
AlfatUber <167688170+AlfatUber@users.noreply.github.com>
2025-05-18 21:39:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def download_video_task(url, quality, download_id, cookie_path=None):
         "quiet": True,
         "postprocessors": [{
             "key": "FFmpegMerger",
-            "preferedformat": "mp4"
+            "preferredformat": "mp4"
         }],
         "http_headers": {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
